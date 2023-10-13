3b8903f1c99ef8650fcb8c19995b2449f5b16984
laure reina <laure.reina@gmail.com>
2023-10-13 17:09:47 +0200

    db connection

--- App/config/config.php
+++ App/config/config.php
@@ -1,10 +1,8 @@
 <?php
 
-return array(
-
-    "db_user" => "root",
-    "db_password" => "root",
-    "db_name" => "Blog",
-    "db_host" => "localhost",
-
-);
+return [
+    "db_user" => 'root',
+    "db_password" => 'root',
+    "db_name" => 'Blog',
+    "db_host" => 'localhost',
+];
