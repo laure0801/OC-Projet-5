ac561b439ef563b63369ed7b376a8b6e1f2f8213
laure reina <laure.reina@gmail.com>
2023-10-13 11:23:08 +0200

    database connection

---
+++ App/Security/database.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Security;
+
+use \PDO;
+
+
+class Database
+{
+    /**
+     * @var
+     */
+    protected $database;
+
+    /**
+     * @return PDO
+     */
+    protected function dbConnect()
+    {
+        if ($this->database === null) {
+            $data = require __DIR__ . './../Config/config.php';
+            return new PDO('mysql:host=' . $data['db_host'] . ';dbname=' . $data['db_name'] . ';charset=utf8',
+                $data['db_user'], $data['db_password'],
+                array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        }
+        return $this->database;
+    }
+
+    /**
+     * @param $sql
+     * @param null $parameters
+     * @param null $binds
+     * @return bool|false|\PDOStatement
+     */
+    protected function sql($sql, $parameters = null)
+    {
+
+        if ($parameters) {
+            $result = $this->dbConnect()->prepare($sql);
+
+                $result->execute($parameters);
+
+            return $result;
+        } else {
+            $result = $this->dbConnect()->query($sql);
+
+            return $result;
+        }
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'efbf7516f2794d76bd12755190fdd57f7f7f75dc',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'efbf7516f2794d76bd12755190fdd57f7f7f75dc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
